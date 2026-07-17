dc66b98ec9df744dd41de2a00ffc331991544298
layla-237 <xlaylacoconutx@gmail.com>
2026-07-17 16:20:09 +0300

    Create footer.php

---
+++ template/footer.php
@@ -0,0 +1,38 @@
+  <footer>
+    <div class="footer-logo">Ǝliceu</div>
+
+    <div class="footer-grid">
+      <div>
+        <h3>Navigare</h3>
+        <a href="licee_general.php">Toate Liceele</a>
+	      <a href="ai/chatbot.php">Chatbot AI</a>
+        <a href="despre.php">Despre noi</a>
+        <a href="contact.php">Contact</a>
+        <a href="login.php#register">Înregistrare</a>
+        <a href="login.php">Autentificare</a>
+      </div>
+
+      <div>
+        <h3>Resurse</h3>
+        <a href="specializari.php">Informații despre profiluri</a>
+        <a href="test.php">Test de îndrumare</a>
+        <a href="evenimente.php">Evenimente</a>
+      </div>
+
+      <div>
+        <h3>Legal</h3>
+        <a href="termeni.php">Termeni și condiții</a>
+        <a href="cookies.php">Cookies</a>
+      </div>
+    </div>
+  </footer>
+  <script src="src/js/dragdrop.js"></script>
+
+  <script src="src/js/liceu.js"></script>
+  <script src="src/js/compare_bar.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/intro.js/minified/intro.min.js"></script>
+  <script src="src/js/tutorial.js"></script>
+</body>
+</html>
+
+
