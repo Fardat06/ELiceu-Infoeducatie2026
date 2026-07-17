16d4c58d2c444f5c333154d8ffb8b4c630c25e7d
Fardat06 <fatimatimi08@gmail.com>
2026-07-17 16:16:07 +0300

    Add files via upload

---
+++ plugin/config.php
@@ -0,0 +1,20 @@
+<?php
+// Database
+if (!defined('DB_DRIVER')) {
+    define('DB_DRIVER', 'mysqli');
+}
+if (!defined('DB_HOSTNAME')) {
+    define('DB_HOSTNAME', 'localhost');
+}
+if (!defined('DB_USERNAME')) {
+    define('DB_USERNAME', 'root');
+}
+if (!defined('DB_PASSWORD')) {
+    define('DB_PASSWORD', '');
+}
+if (!defined('DB_DATABASE')) {
+    define('DB_DATABASE', 'clasa8');
+}
+if (!defined('DB_PREFIX')) {
+    define('DB_PREFIX', 'home_');
+}
