f4dfb4e6624fa5b5affe79b2e543938358b1dcc6
Fardat06 <fatimatimi08@gmail.com>
2026-07-22 22:50:19 +0300

    Update function.php

--- admin/plugin/function.php
+++ admin/plugin/function.php
@@ -11,20 +11,17 @@
      
      function sanitize_output($buffer) {
 
-    // Searching textarea and pre
     preg_match_all('#\<textarea.*\>.*\<\/textarea\>#Uis', $buffer, $foundTxt);
     preg_match_all('#\<pre.*\>.*\<\/pre\>#Uis', $buffer, $foundPre);
 
-    // replacing both with <textarea>$index</textarea> / <pre>$index</pre>
     $buffer = str_replace($foundTxt[0], array_map(function($el){ return '<textarea>'.$el.'</textarea>'; }, array_keys($foundTxt[0])), $buffer);
     $buffer = str_replace($foundPre[0], array_map(function($el){ return '<pre>'.$el.'</pre>'; }, array_keys($foundPre[0])), $buffer);
 
-    // your stuff
     $search = array(
-        '/\>[^\S ]+/s',  // strip whitespaces after tags, except space
-        '/[^\S ]+\</s',  // strip whitespaces before tags, except space
-        '/(\s)+/s' ,      // shorten multiple whitespace sequences
-        '/<!--(.|\s)*?-->/' // Remove HTML comments
+        '/\>[^\S ]+/s',
+        '/[^\S ]+\</s', 
+        '/(\s)+/s' ,      
+        '/<!--(.|\s)*?-->/'
     );
 
     $replace = array(
@@ -42,9 +39,7 @@
     return $buffer;
     }
 
-    /**
- * Citește o setare din home_settings. Rezultatele sunt cache-uite pe durata cererii.
- */
+
 function setting(string $key, string $default = ''): string
 {
     static $cache = null;
