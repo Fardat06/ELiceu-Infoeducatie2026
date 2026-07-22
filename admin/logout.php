9f1de768cd8f32296cc6ffc1e5d4ae94a6eba706
Fardat06 <fatimatimi08@gmail.com>
2026-07-22 21:28:57 +0300

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -1,13 +1,8 @@
 <?php
-    session_start();   //Start the session
+    session_start();   
    
-   
-    // session_unset();   //Unset the data
-    // Unset all of the session variables.
     $_SESSION = array();
     
-    // If it's desired to kill the session, also delete the session cookie.
-    // Note: This will destroy the session, and not just the session data!
     if (ini_get("session.use_cookies")) {
         $params = session_get_cookie_params();
         setcookie(session_name(), '', time() - 42000,
@@ -16,8 +11,8 @@
         );
     }
 
-    session_destroy(); //Destroy th session
-    header('location: index.php'); //Goto index.php page
+    session_destroy(); 
+    header('location: index.php');
     exit(); 
     
 ?>
